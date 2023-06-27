d3f375e4905c55608d33fe88136862fa9e189273
Angelo Ambrósio <angelotosta1105@gmail.com>
2023-06-27 10:01:16 -0300

    projeto em andamento

---
+++ main.js
@@ -0,0 +1,63 @@
+/*document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('btn-buscar-cep').addEventListener('click', function(){
+        const xhttp = new XMLHttpRequest();
+        const cep = document.getElementById('cep').value;
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+        xhttp.open('GET', endpoint);
+        xhttp.send();
+    })
+})*/
+
+$(document).ready(function(){
+    $('#cep').mask('00000-000');
+    
+    $('#btn-buscar-cep').click(function(){
+        const cep = $('#cep').val();
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+        const botao = $(this);
+
+        $(botao).find('i').addClass('d-none');
+        $(botao).find('i').removeClass('d-none');
+
+        /*$.ajax(endpoint).done(function(resposta){
+            const logradouro = resposta.logradouro;
+            const bairro = resposta.bairro;
+            const cidade = resposta.localidade;
+            const estado = resposta.uf;
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+
+            $('#endereco').val(endereco);
+
+            setTimeout(function(){
+                $(botao).find('i').removeClass('d-none');
+                $(botao).find('i').addClass('d-none');
+            }, 4000);
+
+        })*/
+
+        fetch(endpoint)
+        .then(function(resposta){
+            return resposta.json();
+        })
+        .then(function(json){
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+
+            $('#endereco').val(endereco);            
+        })
+        .catch(function(erro){
+            alert("Ocorreu um erro ao buscar o endereço, tente novamente mais tarde.");
+        })
+        .finally(function(){
+            setTimeout(function(){
+                $(botao).find('i').removeClass('d-none');
+                $(botao).find('i').addClass('d-none');
+            }, 1000);
+        })
+    })
+})
+
